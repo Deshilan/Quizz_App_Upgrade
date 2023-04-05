7da35423430d98c56bec5a02fb4a7847749a07b0
Deshilan <janakadeshi@gmail.com>
2023-04-05 02:26:43 +0200

    update -login+main menu

--- app/src/main/java/com/example/quizz_app_upgrade/Main_menu.java
+++ app/src/main/java/com/example/quizz_app_upgrade/Main_menu.java
@@ -3,6 +3,8 @@ package com.example.quizz_app_upgrade;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main_menu extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class Main_menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Button main_menu_to_do_quiz = findViewById(R.id.main_menu_to_do_quiz);
+        Button main_menu_to_make_quiz = findViewById(R.id.main_menu_to_make_quiz);
+        Button main_menu_log_out = findViewById(R.id.main_menu_log_out);
+
+        main_menu_log_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
